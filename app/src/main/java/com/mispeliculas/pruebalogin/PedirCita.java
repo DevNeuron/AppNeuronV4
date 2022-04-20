ac2c6d52b955871e5f2d29e463c86a2a553bd062
JFrankz23 <frankz.king58@icloud.com>
2022-04-20 18:43:14 +0200

    //probando calendario

--- app/src/main/java/com/mispeliculas/pruebalogin/PedirCita.java
+++ app/src/main/java/com/mispeliculas/pruebalogin/PedirCita.java
@@ -3,20 +3,30 @@ package com.mispeliculas.pruebalogin;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class PedirCita extends AppCompatActivity {
 
     Spinner spinner;
-    CalendarView calendario;
-    TextView cita;
+
+    // Control calendario
+
+    EditText dp1;
+    Calendar calendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +34,50 @@ public class PedirCita extends AppCompatActivity {
         setContentView(R.layout.activity_pedir_cita);
 
         spinner = findViewById(R.id.spinner);
-        calendario = findViewById(R.id.calendario);
-        cita = findViewById(R.id.cita);
+
+        dp1= findViewById(R.id.dp);
+        final Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+
+
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                updateCalendar();
+            }
+
+            private void updateCalendar(){
+
+                String format = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+
+                dp1.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        dp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(PedirCita.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+            }
+        });
+
+
+
+
 
         String [] experto = {"Elija un especialista", "Opcion1", "Opcion2", "Opcion3"};
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, experto);
         spinner.setAdapter(adapter);
 
-        calendario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
+
+
+
 
     }
 
