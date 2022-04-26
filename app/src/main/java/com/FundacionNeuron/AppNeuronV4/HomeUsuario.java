d54fa81a775b263bc1b75c52bf906f7d7b42c9e3
DevNeuron <funneuron@gmail.com>
2022-04-26 19:22:24 +0200

    error r solucionado

--- app/src/main/java/com/FundacionNeuron/AppNeuronV4/HomeUsuario.java
+++ app/src/main/java/com/FundacionNeuron/AppNeuronV4/HomeUsuario.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.mispeliculas.pruebalogin.R;
+
 
 public class HomeUsuario extends AppCompatActivity {
 
